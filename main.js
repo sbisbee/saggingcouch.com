75e713a07866346b5fe4aee842c24df6192e8a05
Sam Bisbee <sam@sbisbee.com>
2011-12-23 15:10:06 -0500

    building the toc dynamically on the client

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 $(function() {
+  var toc = $('#toc');
+  var tocBuffer = '<ul>';
+  var inner = false;
+
   $('#exTabs').tabs();
   $('#downloadTabs').tabs().css('border', 'none');
+
+  if(toc[0]) {
+    $('h2, h3').each(function(i) {
+      if(this.nodeName.toLowerCase() === 'h3' && !inner) {
+        inner = true;
+
+        tocBuffer += '<ul>';
+      }
+      else if(this.nodeName.toLowerCase() === 'h2' && inner) {
+        tocBuffer += '</ul>';
+      }
+
+      $(this).attr('id', 'title' + i);
+      tocBuffer += '<li><a href="#title' + i + '">' + $(this).text() + '</a>';
+    });
+
+    $(toc).html(tocBuffer + '</ul>');
+  }
 });
