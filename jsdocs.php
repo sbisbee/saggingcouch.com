fd0ae7eab96cef86ea2a99cc189e88683136d4a9
Sam Bisbee <sam@sbisbee.com>
2011-12-23 16:12:35 -0500

    whoops, forgot to commit each piece

--- jsdocs.php
+++ jsdocs.php
@@ -7,6 +7,9 @@
   <a href="https://gist.github.com/1515106">in a gist</a>.
 </i></p>
 
+<p>
+  For Sag-JS v0.1 (last updated Dec 23, 2011)
+
 <div id="toc"></div>
 
 <p>
@@ -98,6 +101,17 @@
   <code>setDatabase()</code>. Returns <code>undefined</code> if no database has
   been set yet.
 
+<h3>setStaleDefault(bool)</h3>
+
+<p>
+  Returns the sag object, so you can chain it.
+
+<p>
+  If set to <code>true</code> then all GET requests will have the
+  <code>stale=ok</code> query parameter appended to their URL. For example, if
+  set and you run a GET on <code>/mydb/_design/app/_view/count</code> then sag
+  will turn the URL into <code>/mydb/_design/app/_view/count?stale=ok</code>
+
 <h3>get(opts)</h3>
 
 <p>
@@ -126,5 +140,45 @@
   function.
 
 <script src="https://gist.github.com/1515106.js?file=put.js"></script>
+
+<h3>delete(id, rev, callback)</h3>
+
+<p>
+  Performs a DELETE request on a document. You just pass in the document's
+  <code>id</code>, <code>rev</code>, and a <code>callback</code> function.
+
+<script src="https://gist.github.com/1515106.js?file=delete.js"></script>
+
+<h3>head(opts)</h3>
+
+<p>
+  Performs a HEAD request at your database's root. No response body will be
+  returned from Couch, making this a much faster operation than a GET and ideal
+  when you just want to see something's state.
+
+<script src="https://gist.github.com/1515106.js?file=head.js"></script>
+
+<h3>copy(opts)</h3>
+
+<p>
+  Copies a document into a new or existing document. If copying over an
+  existing document, then you must provide the <code>dstRev</code>.
+
+<script src="https://gist.github.com/1515106.js?file=copy.js"></script>
+
+<h3>getSession(callback)</h3>
+
+<p>
+  Makes a GET call to /_session, returning your current user's session
+  information.
+
+<h3>bulk(opts)</h3>
+
+<p>
+  Makes a bulk request. The options are <code>docs</code> (array of documents),
+  <code>callback</code> (the callback function), and the optional
+  <code>allOrNothing</code> boolean.
+
+<script src="https://gist.github.com/1515106.js?file=bulk.js"></script>
 
 <?php require('footer.php'); ?>
