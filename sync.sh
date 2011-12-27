5f7ec9d506f4739b280f46224f4eb8fe09afa5c1
Sam Bisbee <sam@sbisbee.com>
2011-12-27 13:11:58 -0500

    quick script to push/pull files to the server

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# sync.sh by Sam Bisbee <sam@sbisbee.com>
+#
+# Released into the public domain. No warranty.
+#
+# Syncs files from the repo root up into the web root on the server. Makes life
+# easier if you do this with pubkey auth instead of typing your password twice.
+
+REMOTE_HOST='sbisbee.com'
+REMOTE_ROOT='public_html/saggingcouch.com'
+
+# Make sure we pull down any distrib files that we forgot to commit.
+rsync -ave ssh $REMOTE_HOST:$REMOTE_ROOT/distrib/ distrib/
+
+# Push
+rsync -ave ssh --exclude sync.sh --exclude .git --delete . $REMOTE_HOST:$REMOTE_ROOT
