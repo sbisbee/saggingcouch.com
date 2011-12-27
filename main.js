b04c81c9cf408ad1c3b1e4f9da751112bf491fea
Sam Bisbee <sam@sbisbee.com>
2011-12-27 13:02:13 -0500

    tracking tab select events in ga

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+function trackEvent(arr) {
+  if(typeof _gaq === 'object' && typeof _gaq.push === 'function') {
+    _gaq.push(['_trackEvent'].concat(arr));
+  }
+  else {
+    //TODO something else
+    console.log('no');
+  }
+}
+
 $(function() {
   var toc = $('#toc');
   var tocBuffer = '<ul>';
   var inner = false;
 
-  $('#exTabs').tabs();
-  $('#downloadTabs').tabs().css('border', 'none');
+  $('#exTabs').tabs({
+    select: function(e, ui) {
+      trackEvent([
+        'Home Example Tabs',
+        'Select - Tab',
+        $(ui.tab).text()
+      ]);
+    }
+  });
+
+  $('#downloadTabs').tabs({
+    select: function(e, ui) {
+      trackEvent([
+        'Download Tabs',
+        'Select - Tab',
+        $(ui.tab).text()
+      ]);
+    }
+  })
+  .css('border', 'none');
 
   if(toc[0]) {
     $('h2, h3').each(function() {
