2c150746dcf1c83ebf6b08c6f67c5cab4853bf6f
Sam Bisbee <sam@sbisbee.com>
2012-01-20 23:57:53 -0500

    Moving GA tracking to bottom of <head> per their rec.

--- main.js
+++ main.js
@@ -2,31 +2,11 @@ function trackEvent(arr) {
   if(typeof _gaq === 'object' && typeof _gaq.push === 'function') {
     _gaq.push(['_trackEvent'].concat(arr));
   }
-  else {
+  else if(console && console.log) {
     //TODO something else
     console.log('no');
   }
 }
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-15812049-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.come/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-(function (window, document) {
-  var ref = document.getElementsByTagName('script')[0],
-      script = document.createElement('script'),
-      secure = window.location.protocol === 'https:';
-  script.async = true;
-  script.src = secure ? 'https://secure-platform.tiptheweb.org/tip/button.js'
-                      : 'http://platform.tiptheweb.org/tip/button.js';
-  ref.parentNode.insertBefore(script, ref);
-}(this, this.document));
 
 $(function() {
   $('#exTabs').tabs({
