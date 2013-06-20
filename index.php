c79db72c68bf7a422a538166a7bc2ddbb8b91565
Sam Bisbee <sam@sbisbee.com>
2013-06-20 14:54:05 -0400

    Touching up home page

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
   <li>
     <span>Automated testing</span>
-    Every release is automatically tested against CouchDB 1.2.x, BigCouch 0.4, and Cloudant.
+    Every release is automatically tested against CouchDB 1.3.x/1.2.x and Cloudant.
   </li>
 
   <li>
@@ -52,29 +52,5 @@
     <script src="https://gist.github.com/850293.js?file=sag-js-browser-example.html"></script>
   </div>
 </div>
-
-<h2>In the News</h2>
-
-<ul id="press">
-  <li>
-    [2011-04-08]
-    <a href="http://weblog.bocoup.com/storing-php-sessions-in-couchdb">Storing PHP Sessions in CouchDB</a>
-    by Sam Bisbee
-
-  <li>
-    [2011-02-28]
-    <a href="http://blog.tropo.com/2011/02/28/no-more-coworking-lockouts-with-tropo-and-couchdb/">No More Coworking Lockouts with Tropo and CouchDB</a>
-    by Mark Headd
-
-  <li>
-    [2011-01-20]
-    <a href="http://blog.tropo.com/2011/01/20/control-your-tropo-scripts-with-external-events/">Control Your Tropo Scripts with External Events</a>
-    by Mark Headd
-
-  <li>
-    [2010-11-22] 
-    <a href="http://www.sbisbee.com/blog.php?id=1654637551">Benchmarking native PHP caching storage options</a>
-    by Sam Bisbee
-</ul>
 
 <?php require_once('footer.php'); ?>
